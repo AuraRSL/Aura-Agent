097e4fed4a2b8f2ca9c6cf3236da889c013cd4e3
kandeh <alirezaknd@yahoo.com>
2018-02-07 18:08:49 +0330

    fix civilianLayer bug

--- src/viewer/layers/AmboLayers/CivilianLayer.java
+++ src/viewer/layers/AmboLayers/CivilianLayer.java
@@ -5,50 +5,40 @@ import AUR.util.knd.AURWorldGraph;
 import adf.agent.info.WorldInfo;
 import viewer.K_ScreenTransform;
 import viewer.K_ViewerLayer;
-import rescuecore2.standard.entities.Area;
 import rescuecore2.standard.entities.Civilian;
-import rescuecore2.standard.entities.Edge;
 import rescuecore2.standard.entities.StandardEntity;
 
 import java.awt.*;
+import rescuecore2.standard.entities.StandardEntityURN;
 
 /**
  * Created by armanaxh on 12/20/17.
+ * modified by Alireza on Feb. 7, 2018
  */
 
-public class CivilianLayer extends K_ViewerLayer{
+public class CivilianLayer extends K_ViewerLayer {
 
-    @Override
-    public void paint(Graphics2D g2, K_ScreenTransform kst, AURWorldGraph wsg, AURAreaGraph selected_ag) {
-        WorldInfo wi = wsg.wi;
-        g2.setStroke(new BasicStroke(1));
-        g2.setColor(Color.green);
-        for (StandardEntity e : wi.getAllEntities()) {
-            if (e instanceof Civilian) {
-                Civilian c = (Civilian) e;
-                int r = 250;//agent size!
-                g2.fillOval(
-                        kst.xToScreen(c.getX() - r), kst.yToScreen(c.getY() + r),
-                        (int) (2 * r * kst.zoom), (int) (2 * r * kst.zoom)
-//                         (2 * r ), (2 * r )
-                 );
+	@Override
+	public void paint(Graphics2D g2, K_ScreenTransform kst, AURWorldGraph wsg, AURAreaGraph selected_ag) {
+		WorldInfo wi = wsg.wi;
+		g2.setStroke(new BasicStroke(1));
+		g2.setColor(Color.green);
+		for (StandardEntity e : wi.getAllEntities()) {
+			if (e.getStandardURN().equals(StandardEntityURN.CIVILIAN)) {
+				Civilian c = (Civilian) e;
+				if (c.isXDefined() == false || c.isYDefined() == false) {
+					continue;
+				}
+				int r = 250; // civilian radius
+				g2.fillOval(
+						kst.xToScreen(c.getX() - r),
+						kst.yToScreen(c.getY() + r),
+						(int) (2 * r * kst.zoom),
+						(int) (2 * r * kst.zoom)
+				);
 
-            }
-        }
-        g2.setStroke(new BasicStroke(1));
-
-        if (selected_ag != null) {
-            g2.setColor(Color.magenta);
-            g2.setStroke(new BasicStroke(2));
-            Area area = selected_ag.area;
-            for (Edge e : area.getEdges()) {
-                g2.drawLine(
-                        kst.xToScreen(e.getStartX()), kst.yToScreen(e.getStartY()),
-                        kst.xToScreen(e.getEndX()), kst.yToScreen(e.getEndY())
-                );
-            }
-            g2.setStroke(new BasicStroke(1));
-            g2.setColor(Color.black);
-        }
-    }
+			}
+		}
+	}
+	
 }
