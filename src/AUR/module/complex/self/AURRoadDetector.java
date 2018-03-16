1aacc6efcef2917eac3ee904562a7cdfeb655433
Amir Aslan Aslani <aa.aslani_6@yahoo.com>
2018-03-16 21:51:11 +0330

    Clean All Shits!!! :|

--- src/AUR/module/complex/self/AURRoadDetector.java
+++ src/AUR/module/complex/self/AURRoadDetector.java
@@ -43,10 +43,12 @@ public class AURRoadDetector extends RoadDetector {
         public AURRoadDetector(AgentInfo ai, WorldInfo wi, ScenarioInfo si, ModuleManager moduleManager, DevelopData developData) {
                 super(ai, wi, si, moduleManager, developData);
                 this.pathPlanning = moduleManager.getModule("ActionTransport.PathPlanning", "AUR.module.algorithm.AuraPathPlanning");
-                this.clustering = moduleManager.getModule("SampleRoadDetector.Clustering", "AUR.module.algorithm.AURKMeans");
+                this.clustering = moduleManager.getModule("SampleRoadDetector.Clustering", "AUR.module.algorithm.AURBuildingClusterer");
                 
                 this.wsg = moduleManager.getModule("knd.AuraWorldGraph");
-                this.psg = moduleManager.getModule("aslan.PoliceScoreGraph");
+                this.psg = moduleManager.getModule("aslan.PoliceScoreGraph","AUR.util.aslan.AURPoliceScoreGraph");
+                registerModule(this.psg);
+                registerModule(this.clustering);
                 
                 this.result = null;
         }
@@ -54,7 +56,6 @@ public class AURRoadDetector extends RoadDetector {
         @Override
         public RoadDetector updateInfo(MessageManager messageManager) {
                 super.updateInfo(messageManager);
-                
                 return this;
         }
 
@@ -84,7 +85,7 @@ public class AURRoadDetector extends RoadDetector {
                 double score = 0;
                 ArrayList<AURAreaGraph> areaGraph = wsg.getAreaGraph(path);
                 for(AURAreaGraph area : areaGraph)
-                        score += area.pa.getFinalScore();
+                        score += area.getFinalScore();
                 return score;
         }
         
